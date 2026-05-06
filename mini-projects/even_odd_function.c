2448d51ffca213d84a48f6f0e1c170774832d2fa
Ramandeep Singh <ramandeep.singh.codes@gmail.com>
2026-05-06 14:52:42 +0530

    added even_odd_function

---
+++ mini-projects/even_odd_function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void checkEvenOdd(int n) {
+    if(n % 2 == 0) {
+        printf("Even\n");
+    } else {
+        printf("Odd\n");
+    }
+}
+
+int main() {
+    int num;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    checkEvenOdd(num);
+
+    return 0;
+}
