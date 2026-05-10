4f5cd5ef30de63166af3605a4dbc7c5ca5cd06ad
Ramandeep Singh <ramandeep.singh.codes@gmail.com>
2026-05-10 17:35:58 +0530

    count_vowel.c

--- mini-projects/count_vowel.c
+++ mini-projects/count_vowel.c
@@ -5,7 +5,7 @@ int main () {
     scanf("%s", str);
     int count = 0;
     for(int i = 0; str[i] != '\0'; i++){
-        if(str[i] == 'a' || str[i] == 'e'||str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' ){
+        if(str[i] == 'a' || str[i] == 'e'|| str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' ){
     count++;
         }
     }
