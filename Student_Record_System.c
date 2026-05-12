bfa627a0cc09d979ec23cf41a9ca009b49959610
Ramandeep Singh <ramandeep.singh.codes@gmail.com>
2026-05-12 16:41:55 +0530

    Update Student_Record_System.c

--- Student_Record_System.c
+++ Student_Record_System.c
@@ -131,18 +131,18 @@ int main () {
         printf("Enter Choice: ");
         scanf("%d", &choice);
         switch(choice) {
-            case 1: addStudent(); 
-                break;
-            case 2: viewAll(); 
-                break;
-            case 3: searchStudent(); 
-                break;
-            case 4: showStatistics(); 
-                break;
-            case 5: saveToFile(); 
-                break;
-            case 6: printf("Good Bye!!\n"); 
-                break;
+            case 1: addStudent(); break;
+                
+            case 2: viewAll(); break;
+                
+            case 3: searchStudent(); break;
+                
+            case 4: showStatistics(); break;
+             
+            case 5: saveToFile(); break;
+           
+            case 6: printf("Good Bye!!\n"); break;
+                
             default: printf("Invalid!!\n"); 
                 break;
         }
